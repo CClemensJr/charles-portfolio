4b90b4dbec0f1432184e9713b3228d43b1191154
Charles Clemens <c@Charless-MBP.hsd1.wa.comcast.net>
2018-01-14 23:00:53 -0800

    Created seeds files for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem ipsum dolor amet mustache shabby chic palo santo, kinfolk
+    actually 90's snackwave schlitz ethical. Cronut retro try-hard VHS normcore.
+    Portland blog deep v seitan. 3 wolf moon street art blog, narwhal cold-pressed
+    single-origin coffee vaporware squid seitan disrupt. Hexagon freegan ramps
+    paleo shaman stumptown green juice mumblecore coloring book hammock
+    letterpress put a bird on it. Cold-pressed tilde fanny pack blue bottle
+    scenester gentrify yr lomo man bun irony gluten-free iPhone."
+  )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 25
+  )
+end
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "Lorem ipsum dolor amet mustache shabby chic palo santo, kinfolk
+    actually 90's snackwave schlitz ethical. Cronut retro try-hard VHS normcore.
+    Portland blog deep v seitan. 3 wolf moon street art blog, narwhal cold-pressed
+    single-origin coffee vaporware squid seitan disrupt.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+puts "9 portfolio items created"
