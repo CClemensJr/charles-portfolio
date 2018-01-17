d28d8fffda4fd5d37a6ec78267331b0cba970ed4
Charles Clemens <c@Charless-MBP.hsd1.wa.comcast.net>
2018-01-16 23:16:33 -0800

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show] #except allows the resources to be collected for all portfolio actions except show
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' #assign a new path to the show action and rename the prefix
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
