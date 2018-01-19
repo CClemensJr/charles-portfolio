9e987bc850e85d749a236c68e7fc254d77a10525
Charles Clemens <c@Charless-MBP.hsd1.wa.comcast.net>
2018-01-18 22:11:01 -0800

    Created custom action for toggle in blog controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   resources :portfolios, except: [:show] #except allows the resources to be collected for all portfolio actions except show
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' #assign a new path to the show action and rename the prefix
 
