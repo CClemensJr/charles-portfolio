c23ca91f076e1225bed415593d5f255a9bc0be7f
Charles Clemens <c@Charless-MBP.hsd1.wa.comcast.net>
2018-01-22 22:17:14 -0800

    Customized registration, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #Devise allows for custom routing! simply change the view name in the path_names hash
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
   resources :portfolios, except: [:show] #except allows the resources to be collected for all portfolio actions except show
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' #assign a new path to the show action and rename the prefix
