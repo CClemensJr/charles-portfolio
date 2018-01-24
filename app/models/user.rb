6ad45f2310f6715634bd5b1c13bd3ea65d819ee7
Charles Clemens <c@Charless-MacBook-Pro.local>
2018-01-24 13:32:05 -0800

    Completed authentication with attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   # :timeoutable (Allows user to be logged out after a certain time) and :omniauthable (Allows third party logins like facebook or google)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # Make sure the user enters a name
+  validates_presence_of :name
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
