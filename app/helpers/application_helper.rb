e19691c64cdd003463d09d5e4d5a32196720f312
Charles Clemens <c@Charless-MBP.hsd1.wa.comcast.net>
2018-01-29 22:24:51 -0800

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
       (link_to "Login", new_user_session_path)
     end
   end
+
+  # Source is an external website
+  def source_helper(layout_name)
+    # So if the session was initiated from an external source
+    if session[:source]
+      # Create a greeting that will thank the visitor from visiting from the source site
+      greeting = "Welcome to the #{layout_name}. Thanks for visiting me from #{session[:source]}"
+      # Then use a content tag to dynamically create html that dynamically displays a string between p tags that have a css class of source-greeting
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
 end
