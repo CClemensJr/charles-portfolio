8a69f9c4a891ebff0dfd75c7a1d9ffe5cffbba3b
Charles Clemens <c@Charless-MBP.hsd1.wa.comcast.net>
2018-02-13 13:40:20 -0800

    Implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,47 @@ module ApplicationHelper
     end
   end
 
+  # Copyright for footer
   def copyright_generator
     KaimuraiCopyrightTool::Renderer.copyright 'Charles Clemens', 'All rights reserved'
+  end
+
+  # Create an array of hashes that detail the separate nav items
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      }
+    ]
+  end
+  
+  # Returns a set of links based on style and content tag
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 end
