aaf8185ddb854f16a2796e9c58a731fd5df07478
Charles Clemens <c@Charless-MBP.hsd1.wa.comcast.net>
2018-02-18 15:22:39 -0800

    Built twitter parser for tweet links

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,27 @@
 module PagesHelper
+  def twitter_parser tweet
+    # Regular expression that searches for any links in the tweet and removes periods or commas at the end
+    regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    # Take the urls and substitute a link into the tweet
+    tweet.gsub(regex) do |url|
+      "<a href='#{url}' target='_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
