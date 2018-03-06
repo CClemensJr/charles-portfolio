de9307a8d732d3e12a09814db8d2baeea9e5a676
Charles Clemens <c@Charless-MBP.hsd1.wa.comcast.net>
2018-03-05 23:46:46 -0800

    Modified some of the blog files

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    @blogs = Blog.page(params[:page]).per(10)
     @page_title = "Charles Clemens | The Words"
   end
 
